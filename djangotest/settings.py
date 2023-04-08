e3ccf4a7429cf114c4bd9e40dc398b7519b2d7d0
„Andi <ajerni@gmail.com>
2023-04-09 00:22:55 +0200

    strike

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'testapp/static')
+    os.path.join(BASE_DIR, 'testapp/static'),
+    os.path.join(BASE_DIR, 'todo_list/static')
 ]
 
 # Default primary key field type
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import list
 
 def todolist(request):
-    return render(request, 'home_todo.html', {})
+    all_items = list.objects.all
+    return render(request, 'home_todo.html', {'all_items':all_items})
 
 def about(request):
     my_name = "AE"
