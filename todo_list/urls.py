5bb7b3ad2aaa2ea71a988c1ae7a2515f1b01474c
„Andi <ajerni@gmail.com>
2023-04-08 23:08:17 +0200

    startet todo list

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todolist', views.todolist, name='todolist')
+    path('todolist', views.todolist, name='todolist'),
+    path('about', views.about, name='about')
     
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def todolist(request):
-    return render(request, 'todo_list.html', {})
+    return render(request, 'home_todo.html', {})
+
+def about(request):
+    my_name = "AE"
+    return render(request, 'about.html', {'name': my_name})
