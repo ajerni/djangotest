536b50b47270ebf80f32bdc4c3da4dd11217b4df
„Andi <ajerni@gmail.com>
2023-04-09 22:39:09 +0200

    toto add added

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import list
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def todolist(request):
-    all_items = list.objects.all
-    return render(request, 'home_todo.html', {'all_items':all_items})
+
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Itemm has been added to list.'))
+            all_items = list.objects.all
+            return render(request, 'home_todo.html', {'all_items':all_items})
+    else:
+        all_items = list.objects.all
+        return render(request, 'home_todo.html', {'all_items':all_items})
 
 def about(request):
     my_name = "AE"
